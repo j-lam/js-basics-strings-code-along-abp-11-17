e61746ec48a8cf989a38a50a6840b22d7b3fa2ae
Jennifer lam <fortunatejen@gmail.com>
2017-11-19 23:28:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,4 @@
 const currentUser = "Jennifer";
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
